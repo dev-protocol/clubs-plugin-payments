bb8a3777bb1cae353769f2f355aa1e089fccd6a0
Aggre <hiroyuki.aggre@gmail.com>
2024-10-28 23:44:00 +0900

    test

--- src/api/fulfillment.test.ts
+++ src/api/fulfillment.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { expect, describe, it, vi, afterEach, type Mock } from 'vitest'
 import { abi, post, type RequestBody } from './fulfillment'
 import type { APIContext } from 'astro'
