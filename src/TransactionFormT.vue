cf3255673561a95919b697865e75c2a4ccd6fccc
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2024-10-29 21:35:48 +0530

    wip

---
+++ src/TransactionFormT.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>Transaction Form</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
+import type { Failure, Success } from './api/payment-key'
+import type { ComposedItem } from '.'
+import { whenDefined, whenDefinedAll, whenNotError } from '@devprotocol/util-ts'
+import { JsonRpcProvider } from 'ethers'
+import { clientsSTokens } from '@devprotocol/dev-kit'
+import { i18nFactory, mintedIdByLogs } from '@devprotocol/clubs-core'
+import {
+  IconBouncingArrowRight,
+  IconSpinner,
+} from '@devprotocol/clubs-core/ui/vue'
+import { Strings } from './i18n'
+
+export default {
+
+
+    props: {
+        item: Object as () => ComposedItem,
+        chainId: Number,
+        rpcUrl: String,
+        debugMode: Boolean,
+    },
+}
+</script>
