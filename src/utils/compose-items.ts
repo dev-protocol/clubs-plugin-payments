7f4725cee9cfe7be84417b2f22f2bc2be84a0a32
Aggre <hiroyuki.aggre@gmail.com>
2024-11-18 23:25:42 +0900

    support offerings

--- src/utils/compose-items.ts
+++ src/utils/compose-items.ts
@@ -6,12 +6,13 @@ import {
 import { whenDefined, type UndefinedOr } from '@devprotocol/util-ts'
 
 import type { ComposedItem, Override } from '..'
-import type { Membership } from '@devprotocol/clubs-core'
+import type { ClubsConfiguration, Membership } from '@devprotocol/clubs-core'
 import type { Collection } from '../types'
 
 export const composeItems = (
   options: ClubsPluginOptions,
   { getPluginConfigById }: ClubsFactoryUtils,
+  offerings: ClubsConfiguration['offerings'] = [],
 ): ComposedItem[] => {
   const overrides =
     (options.find((opt) => opt.key === 'override')?.value as UndefinedOr<
@@ -20,23 +21,29 @@ export const composeItems = (
 
   const items: ComposedItem[] = overrides
     .map((ov) => {
-      const [sourceConfig] = getPluginConfigById(ov.importFrom)
-      const source = (
-        sourceConfig?.options?.find((op) => op.key === ov.key)?.value as
-          | undefined
-          | Membership[]
-          | Collection[]
-      )?.find((s) => {
-        // Check if the keys from collection object are present in s, if yes then it's a collection.
-        return 'memberships' in s ||
-          'requiredMemberships' in s ||
-          'endTime' in s
-          ? !!(s as Collection).memberships.find(
-              // This is a collection, so find the membership and return boolean.
-              (m) => bytes32Hex(m.payload) === bytes32Hex(ov.payload),
-            )
-          : bytes32Hex(s.payload) === bytes32Hex(ov.payload) // This is a membership so directly compare payload.
-      })
+      const [sourceConfig] = ov.importFrom
+        ? getPluginConfigById(ov.importFrom)
+        : [undefined]
+      const source = sourceConfig
+        ? (
+            sourceConfig.options?.find((op) => op.key === ov.key)?.value as
+              | undefined
+              | Membership[]
+              | Collection[]
+          )?.find((s) => {
+            // Check if the keys from collection object are present in s, if yes then it's a collection.
+            return 'memberships' in s ||
+              'requiredMemberships' in s ||
+              'endTime' in s
+              ? !!(s as Collection).memberships.find(
+                  // This is a collection, so find the membership and return boolean.
+                  (m) => bytes32Hex(m.payload) === bytes32Hex(ov.payload),
+                )
+              : bytes32Hex(s.payload) === bytes32Hex(ov.payload) // This is a membership so directly compare payload.
+          })
+        : offerings?.find(
+            (s) => bytes32Hex(s.payload) === bytes32Hex(ov.payload),
+          )
       const composed = whenDefined(source, (so) => ({ ...ov, source: so }))
       return composed
     })
