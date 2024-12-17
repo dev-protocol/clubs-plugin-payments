9e3dc85b777b11a50f8a9d01b4b8224e62c7a7a4
Aggre <hiroyuki.aggre@gmail.com>
2024-12-17 17:16:22 +0900

    /payment-key API is now support Passport items

---
+++ src/utils/compose-passport-item.ts
@@ -0,0 +1,35 @@
+import { createClient } from 'redis'
+import type { ComposedItem } from '..'
+import { ClubsConfiguration } from '@devprotocol/clubs-core'
+import { checkoutPassportItemForPayload } from '@devprotocol/clubs-plugin-passports'
+import { UndefinedOr, whenDefinedAll } from '@devprotocol/util-ts'
+
+export const composePassportItem = async (
+  payload: Uint8Array | string,
+  {
+    config,
+    client,
+  }: {
+    config: ClubsConfiguration
+    client: Awaited<ReturnType<typeof createClient>>
+  },
+): Promise<UndefinedOr<ComposedItem>> => {
+  const passportItem = await checkoutPassportItemForPayload(payload, {
+    config,
+    client,
+  })
+  return whenDefinedAll(
+    [
+      passportItem,
+      passportItem?.props.discount?.price.yen ??
+        passportItem?.props.fiat.price.yen,
+    ],
+    ([item, yen]) => ({
+      payload,
+      price: {
+        yen,
+      },
+      source: item.props.offering,
+    }),
+  )
+}
