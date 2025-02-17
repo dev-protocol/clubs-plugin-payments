f7f763a02c227ab8452f0981b2008b83b01c143c
Aggre <hiroyuki.aggre@gmail.com>
2025-02-17 09:47:57 +0900

    update i18n dictionary

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,8 +4,8 @@ export const Strings = {
   Email: { en: 'Email', ja: 'メールアドレス' },
   EmailPlaceholder: { en: 'Enter your email', ja: 'あなたのメールアドレス' },
   Account: { en: 'Account', ja: 'アカウント' },
-  FullName: { en: 'Full name', ja: '本名' },
-  FullNamePlaceholder: { en: 'Enter your full name', ja: 'あなたの本名' },
+  FullName: { en: 'Your name', ja: '名前' },
+  FullNamePlaceholder: { en: 'Enter your name', ja: 'あなたの名前' },
   PayWithACreditCard: {
     en: 'Pay with a credit card',
     ja: 'クレジットカードで支払う',
